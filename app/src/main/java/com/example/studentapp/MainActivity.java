3840bd95801725f99822909fba7c8da0ed7b846b
Tat Uyen Tam <58362988+TUT888@users.noreply.github.com>
2022-12-20 20:25:56 +0700

    Merge complete

--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -36,10 +36,11 @@ import com.google.gson.Gson;
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
-//    public final static String URL = "http://192.168.1.8:8282"; // Tam url
+    public final static String URL = "http://192.168.1.8:8282"; // Tam url
 //    public final static String url = "http://10.35.48.79"; ///Tien url
 //    public final static String URL = "http://172.16.12.110"; ///Tien url
 
+    public final static String URL_IMAGE = URL +  "/image/";
     public static String CURRENT_LOGIN_NAME = "";
     public static final String CURRENT_LOGIN_ROLE = "Học viên";
     public static final String PROFILE_FRAGMENT_TAG = "PROFILE_FRAGMENT_TAG";
